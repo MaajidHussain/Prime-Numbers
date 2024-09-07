cee69a4a15b1328a89ed38ea37a41935df1a3a79
Maajid Hussain <ksmaajidhussain@gmail.com>
2024-09-07 21:10:28 +0530

    Add files via upload

---
+++ Prime Numbers1.py
@@ -0,0 +1,13 @@
+def find_prime_numbers(start,end):
+    primes=[]
+    for num in range(start,end+1):
+        if num>1:
+            for i in range(2,int(num**0.5)+1):
+                if num%i==0:
+                    break
+            else:
+                primes.append(num)
+    return primes
+start=int(input("Enter the starting number of interval: "))
+end=int(input("Enter the ending number of interval: "))
+print(find_prime_numbers(start,end))
